fd0ebb40e7d0ca6f2c229f70cb3015ee8ec0abff
Guisheng Huang <45245414+mikehuang123@users.noreply.github.com>
2023-03-21 22:54:09 -0700

    add register function with firebase

---
+++ JavaScript/database.js
@@ -0,0 +1,22 @@
+// tester account: email : tester@gmail.com ; password:123456 ; cTdGDjbHAUFeTna2ygWs
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAoiwpM4SMW2fx1E0RSXCIzMsYIXT84kv4",
+    authDomain: "quizstruction.firebaseapp.com",
+    projectId: "quizstruction",
+    storageBucket: "quizstruction.appspot.com",
+    messagingSenderId: "1012312141205",
+    appId: "1:1012312141205:web:2b4fc0bce20599a9d21a9c"
+};
+
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
+
+
+
+export default db;
+
--- JavaScript/login-page.js
+++ JavaScript/login-page.js
@@ -1,3 +1,6 @@
+import db from "./database.js";
+
+
 let signup = document.querySelector(".signup");
 let login = document.querySelector(".login");
 let slider = document.querySelector(".slider");
@@ -12,3 +15,51 @@ login.addEventListener("click", () => {
     slider.classList.remove("moveslider");
     formSection.classList.remove("form-section-move");
 });
+
+
+
+document.getElementById("signBtn").onclick = async ()=>{
+    // when register clicked
+    console.log("Signup clicked");
+
+    if(document.getElementById("signupName").value == ""){
+        alert("Name must be filled out");
+        return false;
+    }
+
+    if(document.getElementById("signupEmail").value == ""){
+        alert("Email must be filled out");
+        return false;
+    }
+
+    if(document.getElementById("signupPassword1").value == ""){
+        alert("Password must be filled out");
+        return false;
+    }
+
+    if(document.getElementById("signupPassword1").value != document.getElementById("signupPassword2").value){
+        alert("Two password does not matched");
+        return false;
+    }
+
+    // pass all validation - setup database
+    try{
+       db.collection("Account").add({
+            name : document.getElementById("signupName").value,
+            email : document.getElementById("signupEmail").value,
+            password : document.getElementById("signupPassword1").value
+       })
+       .then(()=>{
+            console.log("Account Written");
+       })
+       .catch((e) => {
+            console.log("Error create user... ", e);
+       });
+
+    } catch(e){
+        console.error("Error create user... " , e);
+    }
+
+    
+}
+
